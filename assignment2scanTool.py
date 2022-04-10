8111945fbf8d4c80f6d1331fc4cc7c45e9e31726
NovusIrez <izhampromaco@gmail.com>
2022-04-10 15:18:07 +0800

    Update assignment2scanTool.py

--- assignment2scanTool.py
+++ assignment2scanTool.py
@@ -1,15 +1,17 @@
 from scapy.all import *
 # import nmap
-ans = int(input("1- Determine target OS\n 2- Scan range of IP\n 3- Scan range of port\n Enter a number: "))
+ans = int(input(" 1- Determine target OS\n 2- Scan range of IP\n 3- Scan range of port\n Enter a number: "))
 PH=IP()
 TPH=TCP()
-PH.dst = input("Enter target IP: ")
 if ans == 1:
+    PH.dst = input("Enter target IP: ")
     print("don't know")
 elif ans == 2:
-    smask = input("Enter the subnet mask in '/' eg. /30: ")
+    PH.dst = input("Enter network address : ")
+    smask = input("Enter the subnet mask in slash eg. /30\n ")
     PH.dst = PH.dst + smask
 elif ans == 3:
+    PH.dst = input("Enter target IP: ")
     fp = input("Enter first port: ")
     lp = input("Enter last port: ")
     PH.dport = (fp,lp)
